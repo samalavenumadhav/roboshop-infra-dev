4bf900fb50a6e301c5438087e70078d700dfe43a
venu madhav <madhavsamala171@gmail.com>
2026-03-12 18:46:10 +0530

    A

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -3,7 +3,7 @@ resource "aws_security_group_rule" "bastion_internet" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = [0.0.0.0/0]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = local.bastion_sg_id
 }
 
