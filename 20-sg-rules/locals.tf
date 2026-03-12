4aabc2b868a8f219b3465bd3770bac16a87efc9e
venu madhav <madhavsamala171@gmail.com>
2026-03-12 20:03:19 +0530

    A

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -1,6 +1,6 @@
 locals {
-    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
-    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
-    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-    user_sg_id = data.aws_ssm_parameter.user_sg_id.value
+  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  user_sg_id = data.aws_ssm_parameter.user_sg_id.value
 }
