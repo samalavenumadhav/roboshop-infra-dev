f3296bd7cce05d13a8a6bda0b34ee0a9bb65dbc6
venu madhav <madhavsamala171@gmail.com>
2026-03-12 14:18:47 +0530

    A

--- 00-vpc/output.tf
+++ 00-vpc/output.tf
@@ -5,8 +5,8 @@ output "public_subnet_ids" {
     value = module.vpc.public_subnet_ids
 }
 output "private_subnet_ids" {
-    value = module.vpc.public_subnet_ids
+    value = module.vpc.private_subnet_ids
 }
 output "database_subnet_ids" {
-    value = module.vpc.public_subnet_ids
+    value = module.vpc.database_subnet_ids
 }
