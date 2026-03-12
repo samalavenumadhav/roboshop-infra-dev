f84cb113ae5157ef2d87da9717cbe0085bfe77f1
venu madhav <madhavsamala171@gmail.com>
2026-03-12 20:36:16 +0530

    A

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -1,3 +1,11 @@
+data "http" "my_public_ip_v4" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+output "my_ipv4_address" {
+  value = chomp(data.http.my_public_ip_v4.response_body)
+}
+
 data "aws_ssm_parameter" "bastion_sg_id" {
     name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
