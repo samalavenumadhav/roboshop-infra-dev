8ef0dec9072578e115e8c7eda959c17d7f4d1226
venu madhav <madhavsamala171@gmail.com>
2026-03-13 15:42:59 +0530

    A

---
+++ 30-bastion/locals.tf
@@ -0,0 +1,8 @@
+locals {
+    ami_id = data.aws_ami.joindevops.id
+
+
+    public_subnet_id = data.aws_ssm_parameter.public_subnet_ids.value[0]
+
+    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+}
---
+++ 30-bastion/main.tf
@@ -0,0 +1,16 @@
+# Launch the bastion EC2 instance
+resource "aws_instance" "bastion" {
+  ami                         = local.ami_id
+  instance_type               = "t3.micro"
+  subnet_id                   = local.public_subnet_id
+  vpc_security_group_ids      = [local.bastion_sg_id]
+
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-bastion"
+    },
+    local.common_tags
+  )
+}
+
