df2e759960288f73ff6fceebb8ed5df3a49a28c9
venu madhav <madhavsamala171@gmail.com>
2026-03-13 20:29:29 +0530

    A

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -13,3 +13,31 @@ resource "aws_instance" "bastion" {
     local.common_tags
   )
 }
+
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+    provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+
+# Use the remote-exec provisioner to run the script
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh"
+      
+    ]
+  }
+}
