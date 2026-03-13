8a9e6f54d28b15d030c8fea86f44db91a10b791e
venu madhav <madhavsamala171@gmail.com>
2026-03-13 19:21:23 +0530

    A

---
+++ 40-databases/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "bastion" {
+  ami                         = local.ami_id
+  instance_type               = "t3.micro"
+  subnet_id                   = local.database_subnet_id
+  vpc_security_group_ids      = [local.mongodb_sg_id]
+
+
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-mongodb"
+    },
+    local.common_tags
+  )
+}
