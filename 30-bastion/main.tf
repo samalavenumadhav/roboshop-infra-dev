e533b8eeac9ea66376db6c7bb50f656a564d62c1
venu madhav <madhavsamala171@gmail.com>
2026-03-13 18:32:24 +0530

    A

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -14,3 +14,33 @@ resource "aws_instance" "bastion" {
   )
 }
 
+resource "aws_iam_role" "bastion" {
+  name = "RoboshopDevBastion"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+     tags = merge(
+    {
+        Name = "RoboshopDevBastion"
+    },
+    local.common_tags
+  )
+}
+#Permissions
+/* resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = 
+  policy_arn = aws_iam_policy.policy.arn
+} */
