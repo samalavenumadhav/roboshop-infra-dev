cc21e610848dc485c83afd24b8a05f65076f2bfe
venu madhav <madhavsamala171@gmail.com>
2026-03-18 15:12:12 +0530

    A

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,8 +5,8 @@ environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
-#git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
+git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
 
-#cd ansible-roboshop-roles-tf
-#git pull
-#ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+cd ansible-roboshop-roles-tf
+git pull
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
