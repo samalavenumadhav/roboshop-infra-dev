32d33774288e74d0a8195691482b78ee8946a1d3
venu madhav <madhavsamala171@gmail.com>
2026-03-18 16:23:16 +0530

    A

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -4,3 +4,10 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+variable "zone_id" {
+    default = "Z0935977CBYGA1Y5S3YB"
+}
+variable "domain_name" {
+    default = "samala.online"
+}
+
