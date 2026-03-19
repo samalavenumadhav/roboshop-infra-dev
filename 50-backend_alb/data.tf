4c8db92b928687101ab5aa21a8bbe3e13d6a82e5
venu madhav <madhavsamala171@gmail.com>
2026-03-19 21:17:03 +0530

    A

--- 50-backend_alb/data.tf
+++ 50-backend_alb/data.tf
@@ -1,5 +1,5 @@
 data "aws_ssm_parameter" "backend_alb_sg_id" {
-    name = "${var.project}-${var.environment}-backend_alb_sg_id"
+    name = "${var.project}-${var.environment}/backend_alb_sg_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
