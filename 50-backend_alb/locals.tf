6e8265b94ace862705dfafb4dc0eb0decb5d8e3d
venu madhav <madhavsamala171@gmail.com>
2026-03-19 21:23:44 +0530

    A

--- 50-backend_alb/locals.tf
+++ 50-backend_alb/locals.tf
@@ -1,8 +1,8 @@
+
 locals {
-    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
-    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-    #private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids.value
-    common_tags = {
+  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  common_tags = {
         Project = var.project
         Environment = var.environment
         Terraform = "true"
