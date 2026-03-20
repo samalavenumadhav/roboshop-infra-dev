f6591fa5a9bb3dc68518b4160b5e539bf3a3094e
venu madhav <madhavsamala171@gmail.com>
2026-03-20 13:21:05 +0530

    A

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,7 +2,7 @@ locals {
     ami_id = data.aws_ami.joindevops
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     #private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids.value
-    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
     common_tags = {
         Project = var.project
         Environment = var.environment
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "catalogue" {
   ami           = local.ami_id
   instance_type = "t3.micro"
-  subnet_id = local.private_subnet_ids
+  subnet_id = local.private_subnet_id
   vpc_security_group_ids = [local.catalogue_sg_id]
 
 
