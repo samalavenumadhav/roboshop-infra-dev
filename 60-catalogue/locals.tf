b108f6a3279a6f9534a13b125119b9018feea853
venu madhav <madhavsamala171@gmail.com>
2026-03-20 12:51:02 +0530

    A

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,6 +1,6 @@
 locals {
-    ami_id = data.aws_ami.joindevops.value
-    catalogue_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+    ami_id = data.aws_ami.joindevops
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids.value
 
     common_tags = {
