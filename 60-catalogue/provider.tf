612f170312b3c3ce71e061c3b669ca3fd0c1ffbd
venu madhav <madhavsamala171@gmail.com>
2026-03-20 16:00:25 +0530

    A

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {
       bucket         = "samala-99"
-      key            = "roboshop-dev-backend_alb.tfstate"
+      key            = "roboshop-dev-catalogue"
       region         = "us-east-1"
       encrypt        = true
       use_lockfile = true
