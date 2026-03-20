dc319c0b058dff256784d22517e1585352c0d027
venu madhav <madhavsamala171@gmail.com>
2026-03-20 20:05:35 +0530

    A

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -57,3 +57,120 @@ resource "aws_ami_from_instance" "catalogue" {
     local.common_tags
   )
 }
+
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${var.project}-${var.environment}-catalogue"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60 
+
+  health_check {
+    interval            = 10                  // Time between checks in seconds (range 5-300, default 30 for instance/ip)
+    path                = "/health"            // Destination path for health checks (default /)
+    port                = "8080"       // Port for health checks (default traffic-port)
+    protocol            = "HTTP"               // Protocol for health checks (HTTP, HTTPS, TCP, TLS, GENEVE)
+    timeout             = 2                    // Time without a response means a failure (range 2-120s, default 6s for HTTP)
+    healthy_threshold   = 2                 // Consecutive successes required before healthy (range 2-10, default 5)
+    unhealthy_threshold = 3                    // Consecutive failures required before unhealthy (range 2-10, default 2)
+    matcher             = "200-299"            // Success codes (range 200-499 for HTTP/HTTPS, default 200)
+  }
+}
+
+resource "aws_launch_template" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+
+
+  image_id = aws_ami_from_instance.catalogue.id
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  update_default_version = true 
+
+  tag_specifications {
+    resource_type = "instance"
+
+     tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+}
+    tag_specifications {
+    resource_type = "volume"
+
+     tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+}
+    tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_autoscaling_group" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 2
+  max_size           = 10
+  min_size           = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  force_delete              = false
+
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier       = [local.private_subnet_id]
+  target_group_arns  = [aws_lb_target_group.catalogue.arn]
+
+    instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+  dynamic "tag" {
+  for_each = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+  content {
+    key = tag.key
+    valume = tag.value
+    propagate_at_launch = true
+  }
+ }
+ timeouts {
+   delete = "15m"
+ }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${var.project}-${var.environment}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+    estimatedestimated_instance_warmup = 120
+  target_tracking_configuration" {
+      predefined_metric_specification {
+        predefined_metric_type = "ASGAverageCPUUtilization"
+
+  }
+  target_value = 70.0
+  }
+}
