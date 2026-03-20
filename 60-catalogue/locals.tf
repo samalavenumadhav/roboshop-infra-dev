8ea1671607e2244e084ce6a85ba9f705102ccd30
venu madhav <madhavsamala171@gmail.com>
2026-03-20 21:22:04 +0530

    A

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -4,6 +4,7 @@ locals {
     #private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
     vpc_id = data.aws_ssm_parameter.vpc_id.value
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     common_tags = {
         Project = var.project
         Environment = var.environment
