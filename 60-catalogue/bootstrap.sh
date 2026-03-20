84c2b758cc79c3121caef8481007615a37adc761
venu madhav <madhavsamala171@gmail.com>
2026-03-20 13:44:55 +0530

    A

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -6,7 +6,7 @@ app_version=$3
 dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
+git clone https://github.com/samalavenumadhav/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
