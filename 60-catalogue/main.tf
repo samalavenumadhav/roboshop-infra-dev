970a3d345410336457e8b96f2ffce8d55b2c4f74
venu madhav <madhavsamala171@gmail.com>
2026-03-20 17:26:53 +0530

    A

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -39,7 +39,21 @@ resource "terraform_data" "catalogue" {
   }
 }
 
-# resource "aws_ec2_instance_state" "catalogue" {
-#   instance_id = aws_instance.catalogue.id
-#   state       = "stopped"
-# }
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [terraform_data.catalogue]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.project}-${var.environment}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
+   depends_on = [aws_ec2_instance_state.catalogue]
+  # Optional: Add description, tags, etc.
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+}
