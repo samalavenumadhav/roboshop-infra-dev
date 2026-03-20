a040faadb777ada8e2f0bbbf2f8fa2dcb6fc2c2d
venu madhav <madhavsamala171@gmail.com>
2026-03-20 13:24:27 +0530

    A

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    ami_id = data.aws_ami.joindevops
+    ami_id = data.aws_ami.joindevops.id
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     #private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
