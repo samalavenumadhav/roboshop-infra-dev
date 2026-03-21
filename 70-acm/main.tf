c5156d1398d3c1c809e738c1ef52fe02162298b4
venu madhav <madhavsamala171@gmail.com>
2026-03-21 12:12:46 +0530

    A

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -1,5 +1,5 @@
-resource "aws_acm_certificate" "Roboshop" {
-  domain_name       = "samala.online"
+resource "aws_acm_certificate" "roboshop" {
+  domain_name       = "*.{var.domain_name}"
   validation_method = "DNS"
 
   tags = merge (
