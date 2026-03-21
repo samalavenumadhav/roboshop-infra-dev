b07172e421a2234a75d06375a3d856ecb188337a
venu madhav <madhavsamala171@gmail.com>
2026-03-21 12:08:30 +0530

    A

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -17,7 +17,7 @@ resource "aws_acm_certificate" "Roboshop" {
 
 resource "aws_route53_record" "roboshop" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.roboshop.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
