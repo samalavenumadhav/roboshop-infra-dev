1e444b72373680f1d7557be8092f3c8cff4fa729
venu madhav <madhavsamala171@gmail.com>
2026-03-21 13:53:52 +0530

    A

--- 80-frontend-alb/data.tf
+++ 80-frontend-alb/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "frontend_alb_sg_id" {
 data "aws_ssm_parameter" "public_subnet_ids" {
     name = "/${var.project}/${var.environment}/public_subnet_ids"
 }
+
+data "aws_ssm_parameter" "frontend_alb_certificate_arn" {
+    name = "/${var.project}/${var.environment}/frontend_alb_certificate_arn"
+}
