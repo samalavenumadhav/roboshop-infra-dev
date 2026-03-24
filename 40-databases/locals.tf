6490db8b8e6ce05490afe8cbf491a7b424dc0e3d
venu madhav <madhavsamala171@gmail.com>
2026-03-24 19:39:03 +0530

    A

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -14,7 +14,7 @@ locals {
   mysql_role_name = join("-", [
             for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
         ])
-    mysql_policy_name = join("", [
+  mysql_policy_name = join("", [
             for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
         ])
 }
