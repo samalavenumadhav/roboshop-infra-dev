40d9f49a14a3ef3078b8f239c05895c01da192eb
venu madhav <madhavsamala171@gmail.com>
2026-03-25 15:31:34 +0530

    A

--- 95-cdn/main.tf
+++ 95-cdn/main.tf
@@ -7,7 +7,7 @@ resource "aws_cloudfront_distribution" "roboshop" {
     custom_origin_config {
       http_port = 80
       https_port = 443
-      origin_protocol_policy = "https_only"
+      origin_protocol_policy = "https-only"
       origin_ssl_protocols = ["TLSv1.2","TLSv1.1"]
     }
   }
