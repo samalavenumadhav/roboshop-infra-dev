43c67eb5a74390775aacc53cf312908c75a9e088
venu madhav <madhavsamala171@gmail.com>
2026-03-27 14:22:07 +0530

    A

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -1,22 +1,22 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.33.0"
     }
   }
-    backend "s3" {
-      bucket         = "samala-99"
-      key            = "roboshop-dev-vpc"
-      region         = "us-east-1"
-      encrypt        = true
-      use_lockfile = true
-      force_path_style = true 
-    }
+  backend "s3" {
+    bucket           = "samala-99"
+    key              = "roboshop-dev-vpc"
+    region           = "us-east-1"
+    encrypt          = true
+    use_lockfile     = true
+    force_path_style = true
+  }
 }
 
 provider "aws" {
   # Configuration options
 
-   region ="us-east-1"
+  region = "us-east-1"
 }
