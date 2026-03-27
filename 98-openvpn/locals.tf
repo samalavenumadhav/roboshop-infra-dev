e059a34cf492cfcb5ceea67f337c968157d138e4
venu madhav <madhavsamala171@gmail.com>
2026-03-27 13:59:53 +0530

    A

--- 98-openvpn/locals.tf
+++ 98-openvpn/locals.tf
@@ -2,7 +2,7 @@
 locals {
   ami_id           = data.aws_ami.openvpn
   openvpn_sg_id    = data.aws_ssm_parameter.openvpn_sg_id.value
-  public_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
   common_tags = {
     Project     = var.project
     Environment = var.environment
